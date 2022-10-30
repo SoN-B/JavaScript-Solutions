d16e6c98df23ba7df19b8ffdb40e1d3243720d4c
SON <wjdgh2777@naver.com>
2022-10-30 20:00:03 +0900

    문제 풀이 추가

---
+++ week_01/7.Sparse Arrays.js
@@ -0,0 +1,79 @@
+/**
+ * '문자열배열'과 '쿼리배열'을 입력받아 '쿼리배열'내 요소가 '문자열배열'속 요소와 일치하는 부분이 있는지 찾는 문제
+ */
+
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+    inputString = inputString.split("\n");
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+function matchingStrings(strings, queries) {
+    let result = [];
+    let count = 0;
+
+    for (let x of queries) {
+        for (let y of strings) {
+            if (x === y) count++;
+        }
+        result.push(count);
+        count = 0;
+    }
+
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const stringsCount = parseInt(readLine().trim(), 10);
+
+    let strings = [];
+
+    for (let i = 0; i < stringsCount; i++) {
+        const stringsItem = readLine();
+        strings.push(stringsItem);
+    }
+
+    const queriesCount = parseInt(readLine().trim(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = readLine();
+        queries.push(queriesItem);
+    }
+
+    const res = matchingStrings(strings, queries);
+
+    ws.write(res.join("\n") + "\n");
+
+    ws.end();
+}
