d8a33a2d0fc065ac4e28e939063064b0e87e6bca
SON <wjdgh2777@naver.com>
2022-11-01 20:58:01 +0900

    문제 풀이 추가

---
+++ week_02/1.Lonely Integer.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+    inputString = inputString.split("\n");
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    // Write your code here
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine()
+        .replace(/\s+$/g, "")
+        .split(" ")
+        .map((aTemp) => parseInt(aTemp, 10));
+
+    const result = lonelyinteger(a);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
