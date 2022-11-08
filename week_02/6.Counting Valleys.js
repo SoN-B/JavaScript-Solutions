029d957dd249b12f7d401b900162227cc9f1dd93
SON <wjdgh2777@naver.com>
2022-11-08 23:18:28 +0900

    Comment: 주석 추가

--- week_02/6.Counting Valleys.js
+++ week_02/6.Counting Valleys.js
@@ -1,3 +1,13 @@
+/**
+ * 'U'(up) or 'D'(down)의 연속된 문자들의 배열을 입력으로 받아, 계곡의 수를 세는 문제
+ *
+ * 예시) _(해수면)을 기준으로 내려가는 순간 계곡의 시작을 알리고, 그 후 올라가서 다시 _(해수면)을 만나면 계곡의 끝을 알림
+ *
+ *   _/\      _
+ *(시작)\    /(끝)
+ *       \/\/
+ */
+
 "use strict";
 
 const fs = require("fs");
@@ -32,24 +42,41 @@ function readLine() {
  */
 
 function countingValleys(steps, path) {
-    let level = 0,
-        valley = 0,
-        point = 0;
+    let level = 0, // 현재 높이
+        valley = 0, // 계곡의 수
+        point = 0; // 계곡임을 알리는 변수, 양수(계곡 O), 음수(계곡 X)
 
     for (let step of path) {
         if (step === "U") {
             level++;
             if (point === 1 && level === 0) {
-                point--;
+                // 계곡이었는데, 올라오면서 계곡의 끝을 만남(계곡 +1)
+                point--; // 계곡 종료
                 valley++;
             }
         } else {
             level--;
-            if (point === 0 && level < 0) point++;
+            if (point === 0 && level < 0) point++; // 내려가면서 높이가 음수가 되는 순간 계곡의 시작임
         }
     }
 
     return valley;
+
+    // let altitude = 0;
+    // let valleys = 0;
+    // for (const currentStep of path) {
+    //    if (currentStep === 'U') {
+    //        altitude += 1;
+    //
+    //        if (altitude === 0) { // 단순히 올라오고 높이가 0이 되는 순간만 계곡+1 하면 되는 거였음
+    //            valleys += 1;
+    //        }
+    //    } else {
+    //        altitude -= 1;
+    //    }
+    // }
+    //
+    // return valleys;
 }
 
 function main() {
