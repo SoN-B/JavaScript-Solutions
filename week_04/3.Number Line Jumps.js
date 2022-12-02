1afbfd9941beaf83aa942ea6dd1c2800b6124d63
SON <wjdgh2777@naver.com>
2022-12-02 18:35:48 +0900

    Comment: 주석 추가

--- week_04/3.Number Line Jumps.js
+++ week_04/3.Number Line Jumps.js
@@ -1,3 +1,14 @@
+/**
+ * 캥거루 2마리가 있고, 서로 시작위치 및 칸당 이동 속도가 다른상태에서 서로 같은시간, 같은위치에서 만날 수 있는지 판별하는문제
+ * 
+ * x = 시작위치, v 칸 당 이동속도
+ * Ex) x1 = 2, v1 = 1, x2 = 1, v2 = 2
+ * 
+ * x1 -> 2(시작), 3(1초뒤)
+ * x2 -> 1(시작), 3(1초뒤)
+ * 
+ * 즉, 두 캥거루는 1초뒤 같은위치에서 만난다. YES 출력
+ */
 'use strict';
 
 const fs = require('fs');
@@ -37,13 +48,15 @@ function kangaroo(x1, v1, x2, v2) {
     // Write your code here
     let sum_x1 = x1, sum_x2 = x2;
     
-    if(v1 === v2) return "NO";
+    if(v1 === v2) return "NO"; // 서로 다른 위치에서, 속도가 같으면 절대 못만남
     
     while(true) {
         sum_x1 += v1;
         sum_x2 += v2;
         if(sum_x1 === sum_x2) return "YES";
         if(v1 > v2 && sum_x1 > sum_x2) return "NO";
+        // 다른위치 1번 캥거루의 속도가 더 빠를때, 그 캥거루의 거리 합이 더 커져버리면,
+        // 속도가 느린 2번 캥거루는 그 순간 절대 못 따라 잡습니다.
         else if(v1 < v2 && sum_x1 < sum_x2) return "NO"; 
     }
 }
