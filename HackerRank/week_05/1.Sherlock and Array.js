d2e0d81bec3eb915da43888913d9d04f035003f4
SON <wjdgh2777@naver.com>
2022-12-10 23:44:45 +0900

    문제 풀이 추가

---
+++ HackerRank/week_05/1.Sherlock and Array.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'balancedSums' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function balancedSums(arr) {
+    if(arr.length === 1) return "YES"
+    
+    let left = 0, right = 0;
+    for(let i = 1; i < arr.length; i++) {
+        right += arr[i];
+    }
+    
+    for(let i = 0; i < arr.length; i++) {
+        if(i === arr.length - 1) break;
+        if(left === right) return "YES";
+        
+        left += arr[i];
+        right -= arr[i + 1];
+    }
+    
+    return "NO";
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const T = parseInt(readLine().trim(), 10);
+
+    for (let TItr = 0; TItr < T; TItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+        const result = balancedSums(arr);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
