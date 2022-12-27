9be03688e757f4701f9cd416ebe7e119c01d54d0
SON <wjdgh2777@naver.com>
2022-12-27 20:09:14 +0900

    문제 풀이 추가

---
+++ HackerRank/week_06/4.Recursive Digit Sum.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'superDigit' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING n
+ *  2. INTEGER k
+ */
+function superDigit(n, k) {
+    let super_sum = n;
+    let sum = 0;
+
+    if(10 > n) return super_sum;
+
+    for(let i = 0; i < super_sum.length; i++) sum += parseInt(super_sum[i]);
+    if(k > 0) return superDigit((sum * k).toString(), 0);
+    
+    return superDigit((sum).toString(), 0);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = firstMultipleInput[0];
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    const result = superDigit(n, k);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
