85c63a44429a3a28e72cfd44ecfbf101ff55e644
SON <wjdgh2777@naver.com>
2023-01-19 16:56:10 +0900

    Comment: 주석 추가

--- HackerRank/week_09/4.Repeated String.js
+++ HackerRank/week_09/4.Repeated String.js
@@ -1,3 +1,11 @@
+/**
+ * 무한히 반복되는 영문 소문자의 문자열 s가 있습니다. 정수 n이 주어지면 무한 문자열속 문자 a의 수를 찾아서 인쇄하십시오.
+ * 
+ * Ex) s = 'abcac', n = 11
+ * -> abcacabcaca (n의 크기만큼 원래 문자열들의 요소들을 반복)
+ * 이 문자열속 a의 개수는 5개
+ */
+
 'use strict';
 
 const fs = require('fs');
@@ -31,25 +39,23 @@ function readLine() {
  *  2. LONG_INTEGER n
  */
 function repeatedString(s, n) {
-    let temp = [...s], count = 0;
-    for(let i = 0; i < temp.length; i++) {
-        if(temp[i] === 'a') count++;
-    }
-    
+    let string = [...s], count = 0;
+    // 원래 문자열의 a의 개수를 count;
+    for(let i = 0; i < string.length; i++) if(string[i] === 'a') count++;
+
     let result = 0, divide, mod;
-    divide = parseInt(n / temp.length);
-    result += divide * count;
+    // (반복된 문자열을 포함한 n크기의 문자열속) 원래 문자열이 몇번들어가는지
+    divide = parseInt(n / string.length);
+    result += divide * count; // 반복되는 문자열만큼 count를 곱함
     
-    mod = n % temp.length, count = 0;
-    temp = temp.slice(0,mod);
-    for(let i = 0; i < temp.length; i++) {
-        if(temp[i] === 'a') count++;
-    }
+    mod = n % string.length, count = 0; // 그 후, 남는 문자열 만큼 a를 count
+    for(let i = 0; i < mod; i++) if(string[i] === 'a') count++;
     result += count;
     
-    return result
+    return result;
 }
 
+// 아래코드와 크게 다르지 않음 좀더 읽기 좋은 코드인점
 // function countFrom(string,end){
 //     let total = 0
 //     for(let i =0; i < end; i++){
@@ -58,7 +64,7 @@ function repeatedString(s, n) {
 //     return total
 // }
 
-// // Complete the repeatedString function below.
+// Complete the repeatedString function below.
 // function repeatedString(string, n) {
 //     const numberOfAsInString = countFrom(string,string.length)
 //     const remainder = n % string.length
