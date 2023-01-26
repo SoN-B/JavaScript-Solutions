ec203ea3a4e34dfe45fc079a57630abfc5ee99d9
SON <wjdgh2777@naver.com>
2023-01-26 19:51:41 +0900

    문제 풀이 추가

---
+++ HackerRank/week_10/7.The Grid Search.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gridSearch' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY G
+ *  2. STRING_ARRAY P
+ */
+
+function gridSearch(G, P) {
+    let startIndex;
+    
+    for(let i=0; i<G.length; i++) {
+        for(let j=0; j<G[0].length; j++) {
+            if(G[i].includes(P[0], j)) {
+                startIndex=G[i].indexOf(P[0], j)
+                for(let k=1; k<P.length; k++) {
+                    if(G[i+k].indexOf(P[k], startIndex)!==startIndex){
+                        break;
+                    } else {
+                        if(k==P.length-1){
+                            return 'YES'
+                        }
+                    }
+                } 
+            }
+        }
+    }
+    
+    return 'NO'
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const R = parseInt(firstMultipleInput[0], 10);
+
+        const C = parseInt(firstMultipleInput[1], 10);
+
+        let G = [];
+
+        for (let i = 0; i < R; i++) {
+            const GItem = readLine();
+            G.push(GItem);
+        }
+
+        const secondMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const r = parseInt(secondMultipleInput[0], 10);
+
+        const c = parseInt(secondMultipleInput[1], 10);
+
+        let P = [];
+
+        for (let i = 0; i < r; i++) {
+            const PItem = readLine();
+            P.push(PItem);
+        }
+
+        const result = gridSearch(G, P);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
